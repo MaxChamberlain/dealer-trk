c064d23e8d6f17f3db55b0f2ea249812f5796f14
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-11-06 14:58:25 -0700

    organize

--- src/pages/Documents/components/Steps.jsx
+++ src/pages/Documents/components/Steps.jsx
@@ -15,6 +15,7 @@ const steps = ['Basic Vehicle Information', 'Pricing Information', 'Trade Inform
 
 export default function HorizontalLinearStepper({ activeStep, setActiveStep, newVehicle, setNewVehicle, setCompany, company, companyDetails, setAdding }) {
     const [skipped, setSkipped] = React.useState(new Set());
+    const [loading, setLoading] = React.useState(false);
 
   const isStepOptional = (step) => {
     return false;
@@ -26,7 +27,7 @@ export default function HorizontalLinearStepper({ activeStep, setActiveStep, new
 
   const handleNext = () => {
     if(activeStep === steps.length - 1){
-        setAdding(false);
+        setLoading(true);
         insertDocument(
             {
                 head: company,
@@ -78,73 +79,75 @@ export default function HorizontalLinearStepper({ activeStep, setActiveStep, new
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
+      { loading ? <CircularProgress /> : <>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            if (isStepOptional(index)) {
+              labelProps.optional = (
+                <Typography variant="caption">Optional</Typography>
+              );
+            }
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
             );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-        {activeStep === 0 && <Step1 newVehicle={newVehicle} setNewVehicle={setNewVehicle} company={company} setCompany={setCompany} companyDetails={companyDetails} />}
-        {activeStep === 1 && <Step2 newVehicle={newVehicle} setNewVehicle={setNewVehicle} company={company} setCompany={setCompany} companyDetails={companyDetails} />}
-        {activeStep === 2 && <Step3 newVehicle={newVehicle} setNewVehicle={setNewVehicle} company={company} setCompany={setCompany} companyDetails={companyDetails} />}
-        {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <DocumentItem doc={newVehicle} />
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button 
-            variant='contained'
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-          {console.log(company)}
-            <Button 
-              onClick={handleNext} 
+          })}
+        </Stepper>
+          {activeStep === 0 && <Step1 newVehicle={newVehicle} setNewVehicle={setNewVehicle} company={company} setCompany={setCompany} companyDetails={companyDetails} />}
+          {activeStep === 1 && <Step2 newVehicle={newVehicle} setNewVehicle={setNewVehicle} company={company} setCompany={setCompany} companyDetails={companyDetails} />}
+          {activeStep === 2 && <Step3 newVehicle={newVehicle} setNewVehicle={setNewVehicle} company={company} setCompany={setCompany} companyDetails={companyDetails} />}
+          {activeStep === steps.length ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <DocumentItem doc={newVehicle} />
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button 
               variant='contained'
-              disabled={(
-                activeStep === 0 && newVehicle.v_stock_no && newVehicle.v_make && newVehicle.v_model && newVehicle.v_vin_no) ?
-                false : 
-                (activeStep === 1 && newVehicle.v_margin && newVehicle.v_source && newVehicle.v_days && newVehicle.v_start_price && newVehicle.v_sell_price && newVehicle.v_market_percent && newVehicle.v_initial_mmr && newVehicle.v_initial_carg_h && newVehicle.v_final_mmr && newVehicle.v_final_carg_h) ?
-                false :
-                (activeStep === 2  && company !== 'Select A Company') ?
-                false :
-                true
-              }
-            >
-              {activeStep === steps.length - 1 ? 'Finish' :
-              'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+            {console.log(company)}
+              <Button 
+                onClick={handleNext} 
+                variant='contained'
+                disabled={(
+                  activeStep === 0 && newVehicle.v_stock_no && newVehicle.v_make && newVehicle.v_model && newVehicle.v_vin_no) ?
+                  false : 
+                  (activeStep === 1 && newVehicle.v_margin && newVehicle.v_source && newVehicle.v_days && newVehicle.v_start_price && newVehicle.v_sell_price && newVehicle.v_market_percent && newVehicle.v_initial_mmr && newVehicle.v_initial_carg_h && newVehicle.v_final_mmr && newVehicle.v_final_carg_h) ?
+                  false :
+                  (activeStep === 2  && company !== 'Select A Company') ?
+                  false :
+                  true
+                }
+              >
+                {activeStep === steps.length - 1 ? 'Finish' :
+                'Next'}
+              </Button>
+            </Box>
+          </React.Fragment>
+        )}
+      </>}
     </Box>
   );
 }
