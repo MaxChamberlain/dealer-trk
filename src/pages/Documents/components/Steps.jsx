240c5a55554818e0f9cb7e9314f8c3f15248030e
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-11-06 12:23:40 -0700

    improve

--- src/pages/Documents/components/Steps.jsx
+++ src/pages/Documents/components/Steps.jsx
@@ -5,8 +5,6 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import VehicleDetailsAddition from './VehicleDetailsAddition';
-import { TransitionGroup } from "react-transition-group"
 import DocumentItem from './DocumentItem';
 import { insertDocument } from '../../../utils/api';
 import Step1 from './Steps/Step1';
